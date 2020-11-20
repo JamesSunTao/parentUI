1544525af53434aabad697d75814858436b9026a
zhuenguang <zhuenguang@vipkid.com.cn>
2020-11-20 15:41:01 +0800

    toast dialog

---
+++ examples/components/switch.vue
@@ -0,0 +1,5 @@
+<template>
+<div class="div">
+  <switch></switch>
+</div>
+</template>
---
+++ src/components/switch/src/switch.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>1123</div>
+</template>
+
+<script>
+export default {
+   name: 'switch'
+}
+</script>
+
+<style>
+
+</style>

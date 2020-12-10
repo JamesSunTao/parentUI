95556bedd78dc832cd22d58e7bfbabc623a688bb
yangchangwei <yangchangwei@vipkid.com.cn>
2020-12-10 19:15:41 +0800

    增加与管理系统的数据交互

--- src/components/reportComponents/components/report-achievement/report-achievement.vue
+++ src/components/reportComponents/components/report-achievement/report-achievement.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   .vk-report-achievement-container
     .vk-achievement-bg
-      .vk-achievement-hint
+      .vk-achievement-hint(v-if="hintText" @click="openHint")
       .vk-achievement-rote(:style="roteStyle")
 </template>
 <script>
@@ -22,11 +22,22 @@ const achienementMapping = {
 }
 export default {
   props: {
-    rote: String
+    rote: String,
+    hintText: String
   },
   computed: {
     roteStyle() {
       return {'background-image': `url(${achienementMapping[this.rote]})`}
+    }
+  },
+  methods: {
+    openHint() {
+      this.$dialog({
+        hasCancel: false,
+        textCenter: true,
+        text: this.hintText,
+        confirm: '我知道了'
+      })
     }
   }
 }
@@ -40,6 +51,16 @@ export default {
   position relative
   background-image url('../../asstes/images/head-bg.png')
   background-size cover
+  .vk-achievement-hint
+    display inline-block
+    height 13px
+    width 13px
+    position absolute
+    top 30%
+    right 15%
+    background-image url('../../asstes/images/hint.png')
+    position absolute
+    background-size cover
   .vk-achievement-rote
     width 52px
     height 70px
--- src/components/reportComponents/report/report-head-image/src/reportHeadImage.vue
+++ src/components/reportComponents/report/report-head-image/src/reportHeadImage.vue
@@ -2,7 +2,7 @@
   .vk-report-head-image-container
     .vk-report-head-image(:style="headProps.headStyle")
       headPort(:name="headProps.name" :headImage="headProps.headImage")
-    headAchievement(:rote="headProps.rote")
+    headAchievement(:rote="headProps.rote" :hint-text="headProps.hintText")
     .vk-report-head-title
       headFirstTitle(:title="headProps.firstTitle")
       headSecondTitle(:title="headProps.secondTitle")
@@ -22,6 +22,7 @@ export default {
         left: 20,
         headImage: headImage,
         name: 'name',
+        hintText: '',
         rote: 'aPuls',
         firstTitle: '这是一级标题',
         secondTitle: '这是二级标题',
@@ -30,7 +31,7 @@ export default {
   },
   props: {
     headPropsDes: {
-      type: String,
+      type: Object,
       default: () => {
         return null
       }
@@ -38,7 +39,7 @@ export default {
   },
   created() {
     if (this.headPropsDes) {
-      this.headProps = {...headPropsDes}
+      this.headProps = Object.assign(this.headProps, this.headPropsDes)
     }
   },
   computed: {

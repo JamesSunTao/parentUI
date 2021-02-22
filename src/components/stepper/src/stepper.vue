4bb902559a296f42be42be4e3ab9cb6a2812cf7b
liufang11 <liufang11@vipkid.com.cn>
2021-02-22 14:12:50 +0800

    long_press

--- src/components/stepper/src/stepper.vue
+++ src/components/stepper/src/stepper.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="vk-stepper">
-    <button :style="buttonStyleObj" :class="buttonClassObj.concat('vk-stepper__minus')" :disabled="disabled || disable_minus" @click="handleMinus">-</button>
+    <button @touchstart="onTouchStart('minus')" @touchend="onTouchEnd" :style="buttonStyleObj" :class="buttonClassObj.concat('vk-stepper__minus')" :disabled="disabled || disable_minus">-</button>
     <input :style="inputStyleObj" class="vk-stepper__input" type="text" v-model="value" :disabled="disabled || disable_input" :name="name" @change="handleInputChange"/>
-    <button :style="buttonStyleObj" :class="buttonClassObj.concat('vk-stepper__plus')" @click="handlePlus" :disabled="disabled || disable_plus">+</button>
+    <button @touchstart="onTouchStart('plus')" @touchend="onTouchEnd" :style="buttonStyleObj" :class="buttonClassObj.concat('vk-stepper__plus')" :disabled="disabled || disable_plus">+</button>
   </div>
 </template>
 <script>
-
+const LONG_PRESS_INTERVAL = 200
+const LONG_PRESS_START_TIME = 600
 export default {
   name: "stepper",
   props: {
@@ -83,7 +84,12 @@ export default {
       value: (this.vModel || this.defaultValue).toFixed(Number(this.decimalLength)),
       disable_minus: this.disableMinus,
       disable_plus: this.disablePlus,
-      disable_input: this.disableInput
+      disable_input: this.disableInput,
+      // timer: null,
+      // timer2: null,
+      isLongPress: false,
+      type: '',
+      longPressTimer: null
     };
   },
   mounted() {
@@ -93,6 +99,9 @@ export default {
     if (!this.disablePlus && this.max <= this.value) {
       this.disable_plus = true
     }
+    window.addEventListener('contextmenu', function (e) {
+      e.preventDefault()
+    })
   },
   computed: {
     buttonClassObj: function() {
@@ -100,9 +109,58 @@ export default {
         'vk-stepper__button__' + this.theme,
         {'vk-stepper__button': true}
       ]
+    },
+    getListeners: function() {
+      return createListeners()
     }
   },
   methods: {
+    longPressStep() {
+      const that = this
+      this.longPressTimer = setTimeout(() => {
+        that.onChange();
+        that.longPressStep();
+      }, LONG_PRESS_INTERVAL);
+    },
+    onTouchStart(type){
+      if (this.longPress) {
+        this.isLongPress = false;
+        clearTimeout(this.longPressTimer);
+        const that = this
+        this.longPressTimer = setTimeout(() => {
+          that.isLongPress = true;
+          this.type = type
+          that.onChange();
+          that.longPressStep();
+        }, LONG_PRESS_START_TIME);
+      }
+    },
+    onTouchEnd(event) {
+      if (this.longPress) {
+        clearTimeout(this.longPressTimer);
+        if (this.isLongPress) {
+          this.preventDefault(event);
+        }
+      }
+    },
+    preventDefault(event, isStopPropagation) {
+      if (typeof event.cancelable !== 'boolean' || event.cancelable) {
+        event.preventDefault();
+      }
+      if (isStopPropagation) {
+        this.stopPropagation(event);
+      }
+    },
+    stopPropagation(event) {
+      event.stopPropagation();
+    },
+    onChange() {
+      if(this.type==='minus') {
+        this.handleMinus()
+      } else {
+        this.handlePlus()
+      }
+    },
     handleMinus() {
       this.value = Math.max(+this.value - +this.step, this.min).toFixed(Number(this.decimalLength))
       if (!this.disable_minus && +this.value === +this.min) {
@@ -137,9 +195,16 @@ export default {
     border 0
     font-size 14px
     text-align center
+  .vk-stepper__minus
+    border-radius 4px 0 0 4px
+  .vk-stepper__plus
+    border-radius 0 4px 4px 0
   .vk-stepper__button
     padding 0
     margin 0
     border 0
     font-size 14px
+    user-select none
+    &__round
+      border-radius 50%
 </style>
